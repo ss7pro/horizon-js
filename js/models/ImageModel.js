ef83460002a9a4d5643e558e5a33c3ead6607b41
ss7pro <ss7pro@gmail.com>
2012-06-24 02:32:59 +0200

    reident Images model

--- js/models/ImageModel.js
+++ js/models/ImageModel.js
@@ -1,41 +1,40 @@
 var Image = Backbone.Model.extend({
-    sync: function(method, model, options) {
-           switch(method) {
-               case "read":
-                   JSTACK.Nova.getimagedetail(model.get("id"), options.success);
-                   break;
-               case "delete":
-                   JSTACK.Nova.deleteimage(model.get("id"), options.success);
-                   break;
-               case "update":
-                	console.log("Updating Image: " + model.get("name"));
-                	JSTACK.Nova.updateimage(model.get("id"), model.get("name"), options.success);
-                	break;
-           }
+	sync: function(method, model, options) {
+	switch(method) {
+		case "read":
+			JSTACK.Nova.getimagedetail(model.get("id"), options.success);
+		break;
+	case "delete":
+		JSTACK.Nova.deleteimage(model.get("id"), options.success);
+		break;
+	case "update":
+		console.log("Updating Image: " + model.get("name"));
+		JSTACK.Nova.updateimage(model.get("id"), model.get("name"), options.success);
+		break;
+	}
    },
    
-   parse: function(resp) {
-        if (resp.image != undefined) {
-            return resp.image;
-        } else {
-            return resp;
-        }
-    }
+	parse: function(resp) {
+		if (resp.image != undefined) {
+			return resp.image;
+		} else {
+		return resp;
+		}
+	}
 });
 
-	var Images = Backbone.Collection.extend({
-    	model: Image,
+var Images = Backbone.Collection.extend({
+	model: Image,
     
-	    sync: function(method, model, options) {
-	        switch(method) {
-	            case "read":
+	sync: function(method, model, options) {
+		switch(method) {
+			case "read":
 	                JSTACK.Nova.getimagelist(true, options.success);
 	                break;
 	        }
-	    },
+	},
     
-    parse: function(resp) {
-        return resp.images;
-    }
-    
+	parse: function(resp) {
+		return resp.images;
+	}
 });
