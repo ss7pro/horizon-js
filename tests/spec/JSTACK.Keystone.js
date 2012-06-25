f41fcbc19d18929a365a8c3647860f898d32f943
ss7pro <ss7pro@gmail.com>
2012-06-25 20:43:46 +0200

    implement getregionlit in JSTACK

--- tests/spec/JSTACK.Keystone.js
+++ tests/spec/JSTACK.Keystone.js
@@ -19,4 +19,13 @@ describe("JSTACK Keystone tests", function() {
 		expect(JSTACK.Keystone.params.token).toEqual('146282a8cc8742298dd9b03ed3c3d616');
 		expect(JSTACK.Keystone.params.access).toEqual(JSON.parse(fixture).access);
 	});
+
+	it("Should provide region list", function() {
+		var fixture = this.fixtures.respKeystoneEndpoints;
+        JSTACK.Keystone.params.currentstate = JSTACK.Keystone.STATES.AUTHENTICATED;
+        JSTACK.Keystone.params.access = JSON.parse(fixture).access;
+        var regions = JSTACK.Keystone.getregionlist();
+        expect(regions[0].name).toEqual("r4cz2");
+        expect(regions[1].name).toEqual("r4cz1");
+	});
 });
