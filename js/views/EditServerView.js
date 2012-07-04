bcd46d17a043dbd65048496bfa7d2d4094b327c4
ss7pro <ss7pro@gmail.com>
2012-07-04 14:21:58 +0200

    implement server description edit in EditServerView.js

--- js/views/EditServerView.js
+++ js/views/EditServerView.js
@@ -12,12 +12,17 @@ var EditServerView = CreateModalView.extend({
     getInputsAndValidate: function () {
         var invalid = [];
         var serverName = this.getInputVal("server_name");
+        var serverDescription = this.getInputVal("server_description");
         var serverId = this.getInputVal("instance_id");
         if (serverName == undefined || !serverName.length) {
             invalid.push("serverName");
         }
+        if (serverDescription == undefined) {
+            serverDescription = "";
+        }
         var ret = {
                 serverName: serverName,
+                serverDescription : serverDescription,
                 serverId: serverId
             };
         if (invalid.length) {
@@ -27,15 +32,56 @@ var EditServerView = CreateModalView.extend({
     },
 
     editServer: function (inputs) {
+        var curServer = this.options.params.instanceModel.get(inputs.serverId);
+        var changeSet = {};
+        if (curServer == undefined) {
+            //no such instance
+            return;
+        }
+        if (curServer.get("name") != inputs.serverName) {
+            changeSet["update"] = true;
+        }
+        if (curServer.get("metadata") != undefined) {
+            if (curServer.get("metadata").hasOwnProperty("description")) {
+                if (curServer.get("metadata").description !=
+                    inputs.serverDescription)
+                    changeSet["meta"] = true;
+            } else if (inputs.serverDescription.length)
+                changeSet["meta"] = true;
+        } else{
+            if (inputs.serverDescription.length)
+                changeSet["meta"] = true;
+                
+        } 
         var server = new Instance ();
         server.set({
                     name: inputs.serverName,
+                    meta: {"description" : inputs.serverDescription},
                     id: inputs.serverId
             });
-        server._action("update", {success: function(model,response) {
-                                            UTILS.Events.resetModelByName("instanceModel");
-                                        }
-                                    });
+        var modelsToReset = ["instanceModel"];
+//update name
+        if (changeSet.hasOwnProperty("update")) {
+            var reqId = UTILS.Events.genRequestId();
+            UTILS.Events.setRequestProperty(reqId, "modelsToReset",
+                                            modelsToReset);
+            UTILS.Events.setRequestProperty(reqId, "description", "Server " +
+                                                "name edit request for: "  +
+                                                inputs.serverName);
+            server._action('update', UTILS.Events.requestHandlerDict(reqId));
+        }
+
+//update metadata
+        if (changeSet.hasOwnProperty("meta")) {
+            var reqId = UTILS.Events.genRequestId();
+            UTILS.Events.setRequestProperty(reqId, "modelsToReset",
+                                            modelsToReset);
+            UTILS.Events.setRequestProperty(reqId, "description", "Server " +
+                                            "meta edit request for: "  +
+                                                inputs.serverName);
+            server._action('updatemeta',
+                            UTILS.Events.requestHandlerDict(reqId));
+        }
     }
 
 });
