bfe02ec556d105daf742002f8ef4fbc3bc775d1c
Roman Tatar <romantatar@gmail.com>
2012-07-23 01:16:57 +0200

    payment endpoint communication

--- js/models/PaymentModel.js
+++ js/models/PaymentModel.js
@@ -2,12 +2,23 @@ var PaymentModel = Backbone.Model.extend({
   urlRoot: function() {
     return MODULES.R4C.Config.get('paymentEndpoint')
   },
+  idAttribute: 'payment_id',
   defaults: {
-    code: '',
-    tenant_id: '',
-    token: '',
-    value: '',
-    used_at: ''
+    'payment_id': null,
+    'amount': '',
+    'tenant_api_id': '',
+    'session_id': '',
+    'first_name': '',
+    'last_name': '',
+    'invoice': '',
+    'company_name': '',
+    'street': '',
+    'post_code': '',
+    'city': '',
+    'nip': ''
+  },
+  parse: function(resp) {
+    if(resp.response) return resp.response;
+    return resp;
   }
-
 });
--- js/views/PaymentFormView.js
+++ js/views/PaymentFormView.js
@@ -4,11 +4,102 @@ var PaymentFormView = Backbone.View.extend({
 
   initialize: function() {
     this.loginModel = this.options.loginModel;
+    this.model = new PaymentModel();
+    this.model.set('tenant_api_id', this.loginModel.get('tenant').id);
+    //this.model.on('all', this.debug, this);
+    this.model.on('change:invoice', this.changeInvoice, this);
+    this.model.on('sync' , this.saveSuccess, this);
+    this.model.on('error', this.saveError,   this);
+  },
+
+  events: {
+    'change input'               : 'updateModel',
+    'change select'              : 'updateModel',
+    'keyup input[type=text]'     : 'updateModel',
+    'click #btn-payment'         : 'doPayment' 
   },
 
   render: function() {
     var self = this;
     self.$el.html(self._template());
+    self.updateView();
+    self.changeInvoice();
     return this;
-  }
+  },
+
+  debug: function() {
+    console.log(arguments);
+  },
+
+  updateModel: function(e) {
+    if(e.target.type == 'checkbox') {
+      this.model.set(e.target.name, e.target.checked ? e.target.value : '');
+    } else {
+      this.model.set(e.target.name, e.target.value);
+    }
+  },
+
+  updateView: function() {
+    _.each(this.model.attributes, function(value, name) {
+      name = MODULES.R4C.jqEscape(name);
+      var $el = this.$('[name="' + name + '"]');
+      if($el.attr('type') == 'radio') {
+        value = MODULES.R4C.jqEscape(value);
+        $el.filter('[value="' + value + '"]').attr('checked', true);
+      } else if($el.attr('type') == 'checkbox') {
+        $el.attr('checked', value == '1');
+      } else {
+        $el.val(value);
+      }
+    }, this);
+  },
+
+  changeInvoice: function() {
+    this.$('.invoice').attr('disabled', this.model.get('payment[invoice]') != '1');
+  },
+
+  doPayment: function(e) {
+    e.preventDefault();
+    this.model.set('payment_id', null);
+    this.model.save();
+  },
+
+  saveSuccess: function(model, resp) {
+    this.clearErrors();
+    console.log(['success', model.attributes]);
+  },
+
+  saveError: function(model, resp) {
+    if(resp.responseText) {
+      try {
+        var resp = JSON.parse(resp.responseText);
+        if(resp.errors && resp.errors.fields) {
+          this.clearErrors();
+          _.each(resp.errors.fields, function(msg, name){
+            this.setError(name, msg);
+          }, this);
+          return;
+        }
+      } catch(err) {
+        console.log(err)
+      }
+    }
+    console.log(resp);
+  },
+
+  clearErrors: function() {
+    this.$('.error').removeClass('error');
+    this.$('span.help').text('');
+  },
+
+  setError: function(name, msg) {
+    name = MODULES.R4C.jqEscape(name);
+    this.$('[name="' + name + '"]')
+      .parents('.control-group')
+      .addClass('error')
+      .find('span.help')
+      .text(msg);
+  },
+
+
 });
